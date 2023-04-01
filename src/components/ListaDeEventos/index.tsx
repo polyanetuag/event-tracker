3384618967424ed4e0d8c8808520a4e98ebadde4
Polyane Tuag <pollyportugues@hotmail.com>
2023-04-01 11:24:39 -0300

    refactor: remove function

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -7,9 +7,8 @@ import style from "./ListaDeEventos.module.scss";
 
 const ListaDeEventos: React.FC<{
   aoAlterarStatus: (id: number) => void;
-  aoDeletarEvento: (id: number) => void;
   aoFiltroAplicado: (data: Date | null) => void;
-}> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+}> = ({  aoAlterarStatus, aoFiltroAplicado }) => {
   const eventos = useRecoilValue(listaDeEventosState);
 
   return (
@@ -19,7 +18,6 @@ const ListaDeEventos: React.FC<{
         {eventos.map((evento) => (
           <Evento
             aoAlterarStatus={aoAlterarStatus}
-            aoDeletarEvento={aoDeletarEvento}
             evento={evento}
             key={evento.id}
           />
